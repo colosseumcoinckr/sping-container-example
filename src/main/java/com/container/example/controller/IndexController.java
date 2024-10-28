8ccabde32d94276d13dcbe1e1fee9f5e2d96c0aa
김한슬 <hskim@colosseum.kr>
2024-10-28 10:15:10 +0900

    first commit

---
+++ src/main/java/com/container/example/controller/IndexController.java
@@ -0,0 +1,13 @@
+package com.container.example.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class IndexController {
+
+    @GetMapping("/")
+    public String index() {
+        return "It works!";
+    }
+}
