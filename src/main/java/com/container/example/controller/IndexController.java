96be81d0819b08199828d6901d5cd9a2bec3af67
김한슬 <hskim@colosseum.kr>
2024-11-02 18:50:14 +0900

    test

--- src/main/java/com/container/example/controller/IndexController.java
+++ src/main/java/com/container/example/controller/IndexController.java
@@ -1,13 +1,30 @@
 package com.container.example.controller;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class IndexController {
 
+    private final ApplicationContext application;
+    private final String text;
+
+    public IndexController(ApplicationContext application, @Value("${text}") String text) {
+        this.application = application;
+        this.text = text;
+    }
+
     @GetMapping("/")
     public String index() {
-        return "It works!";
+        return "It works! (" + text + ")";
+    }
+
+    @GetMapping("/shutdown")
+    public void shutdown() {
+        int exitCode = SpringApplication.exit(application, () -> 0);
+        System.exit(exitCode);
     }
 }
